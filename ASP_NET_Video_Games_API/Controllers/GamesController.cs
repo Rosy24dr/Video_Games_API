dcabe8eabca2ec126dd111e9691dc97091498ef1
Lizeth <mendezlizeth084@gmail.com>
2022-05-12 11:25:12 -0700

    Created GamesController File + Working on creating Postman Requests

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,33 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    // api/examples
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            return Ok(videoGamePublishers);
+        }
+
+
+
+
+
+    }
+}
